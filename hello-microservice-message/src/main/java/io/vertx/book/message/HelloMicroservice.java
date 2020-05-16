2667a7251e4460a0c9406f7e74fa3d99eb73c124
Parichay Banerjee <gotopbmail@gmail.com>
2020-05-16 23:37:10 +0530

    message based consumer added

---
+++ hello-microservice-message/src/main/java/io/vertx/book/message/HelloMicroservice.java
@@ -0,0 +1,12 @@
+package io.vertx.book.message;
+
+import io.vertx.core.AbstractVerticle;
+
+public class HelloMicroservice extends AbstractVerticle {
+
+    @Override
+    public void start() {
+
+    }
+
+}
